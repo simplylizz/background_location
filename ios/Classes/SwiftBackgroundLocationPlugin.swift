739c9e8b3c052c7e0ac9e533fc96414eee189f44
Anton Yanchenko <simplylizz@gmail.com>
2024-04-22 22:15:56 +0200

    Disable location indicator

--- ios/Classes/SwiftBackgroundLocationPlugin.swift
+++ ios/Classes/SwiftBackgroundLocationPlugin.swift
@@ -6,10 +6,10 @@ public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationM
     static var locationManager: CLLocationManager?
     static var channel: FlutterMethodChannel?
     var running = false
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = SwiftBackgroundLocationPlugin()
-        
+
         SwiftBackgroundLocationPlugin.channel = FlutterMethodChannel(name: "com.almoullim.background_location/methods", binaryMessenger: registrar.messenger())
         registrar.addMethodCallDelegate(instance, channel: SwiftBackgroundLocationPlugin.channel!)
         SwiftBackgroundLocationPlugin.channel?.setMethodCallHandler(instance.handle)
@@ -25,19 +25,19 @@ public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationM
 
             SwiftBackgroundLocationPlugin.locationManager?.allowsBackgroundLocationUpdates = true
             if #available(iOS 11.0, *) {
-                SwiftBackgroundLocationPlugin.locationManager?.showsBackgroundLocationIndicator = true;
+                SwiftBackgroundLocationPlugin.locationManager?.showsBackgroundLocationIndicator = false;
             }
             SwiftBackgroundLocationPlugin.locationManager?.pausesLocationUpdatesAutomatically = false
         }
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         SwiftBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: "method")
 
         if (call.method == "start_location_service") {
             initLocationManager()
             SwiftBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: "start_location_service")
-            
+
             let args = call.arguments as? Dictionary<String, Any>
             let distanceFilter = args?["distance_filter"] as? Double
             let priority = args?["priority"] as? Int
@@ -74,13 +74,13 @@ public class SwiftBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLocationM
             result(true)
         }
     }
-    
+
     public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
-           
+
         }
     }
-    
+
     public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = [
             "speed": locations.last!.speed,
